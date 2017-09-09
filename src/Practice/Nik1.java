7d7229683d25cbcd0e4ede47140f2049c1b71e45
Nikhil <nikhil@selenium.com>
2017-09-09 19:29:36 +0530

    This is my second project

---
+++ src/Practice/Nik1.java
@@ -0,0 +1,30 @@
+package Practice;
+
+public class Nik1 
+{
+	int i=10;
+	int j=10;
+	 int x;
+	
+	public void add()
+	{
+		x=i+j;
+		
+		//System.out.println(x);
+		
+	}
+
+	public static void main(String[] args) 
+	{
+	    Nik1 ref = new Nik1();
+	    
+		System.out.println(ref.x);
+		//Nik1 ref = new Nik1();
+		//ref.i;
+		//add();
+	    
+		
+	
+	}
+
+}
